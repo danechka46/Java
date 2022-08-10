058dd471522b6920a0aa7e3467df7b19a6d4a248
danechka46 <arzhanov12ab@mail.ru>
2022-08-10 15:40:28 +0300

    add task 1

---
+++ task1/src/Calculator.java
@@ -0,0 +1,25 @@
+/**
+ * @author darzhanov
+ */
+
+public class Calculator {
+    /**
+     * @param n1 the first number for calculate
+     * @param n2 the second number for calculate
+     * @return result of calculate
+     */
+    public double multiply(double n1, double n2) {
+        return n1 * n2;
+    }
+    public double add(double n1, double n2) {
+        return n1 + n2;
+    }
+    public double devide(double n1, double n2) {
+        return n1 / n2;
+    }
+    public double minus(double n1, double n2) {
+        return n1 - n2;
+    }
+
+}
+
